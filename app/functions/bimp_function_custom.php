3d1c7e9f963c0bb23bc85f037c6798b547b28a9a
bimp-software <bimp.software@gmail.com>
2025-06-23 23:51:21 -0400

    Mejorando framework

--- app/functions/bimp_function_custom.php
+++ app/functions/bimp_function_custom.php
@@ -24,31 +24,6 @@ function generarQR($texto = '') {
     return 'data:image/png;base64,' . base64_encode($imageData);
 }
 
-
-/**================================================================================================== */
-/**
- * Regresa el rol del usuario
- * @return mixed
- */
-function get_user_role(){ return $rol = get_user('rol'); }
-function get_default_roles() { return ['Administrador', 'Root']; }
-function is_root($rol) { return in_array($rol, ['Root']); }
-function is_admin($rol) { return in_array($rol, ['Administrador','Root']); }
-function is_profesor($rol) { return in_array($rol, ['Profesor','Administrador','Root']); }
-function is_director($rol) { return in_array($rol, ['Director','Administrador','Root']); }
-function is_estudiante($rol) { return in_array($rol, ['Estudiante','Administrador','Root']); } 
-function is_apoderado($rol) { return in_array($rol, ['Apoderado','Administrador','Root']); } 
-
-function is_user($rol, $roles_aceptados){
-    $default = get_default_roles();
-    
-    if(!is_array($roles_aceptados)){
-        array_push($default, $roles_aceptados);
-        return in_array($rol, $default);
-    } 
-    return in_array($rol, array_merge($default, $roles_aceptados));
-}
-
 function get_notificaciones($index = 0){
     $notificaciones = [
         'Acceso no autorizado.',
