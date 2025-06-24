72d4fedf962f488da8feb46ff16b70a34ff4325e
bimp-software <bimp.software@gmail.com>
2025-06-23 22:51:19 -0400

    cambios menores

--- templates/views/dashboard/indexView.php
+++
@@ -1,7 +0,0 @@
-<?php require_once INCLUDES.'dashboard/inc_header.php'; ?>
-
-    <?php if(is_admin(get_user_role())): ?>
-        <?php require_once COMPONENTS.'dashboard/admin/dash_admin.php'; ?>
-    <?php endif; ?>
-
-<?php require_once INCLUDES.'dashboard/inc_footer.php'; ?>
